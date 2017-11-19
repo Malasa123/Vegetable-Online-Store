c0d14ef5dc6b58ca3c67c87f43dfe19e83bab1d0
Malasa123 <30768444+Malasa123@users.noreply.github.com>
2017-11-19 16:37:59 +0200

    Add files via upload

---
+++ FireConfig/Fire.config.ts
@@ -0,0 +1,10 @@
+
+export const FireConfig = 
+{
+apiKey: "AIzaSyALsEOuO4ivACMdRpwZ8K-eHvMBsfTFUPU",
+authDomain: "tamat-216ac.firebaseapp.com",
+databaseURL: "https://tamat-216ac.firebaseio.com",
+projectId: "tamat-216ac",
+storageBucket: "tamat-216ac.appspot.com",
+messagingSenderId: "137168516561"
+}
