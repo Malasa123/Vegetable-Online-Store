e3be834053dbc8468d49712c64fbb909028b9536
Malasa123 <30768444+Malasa123@users.noreply.github.com>
2017-12-19 18:12:27 +0200

    Hide Configuration

--- FireConfig/Fire.config.ts
+++ FireConfig/Fire.config.ts
@@ -1,10 +1,10 @@
 
 export const FireConfig = 
 {
-apiKey: "AIzaSyALsEOuO4ivACMdRpwZ8K-eHvMBsfTFUPU",
-authDomain: "tamat-216ac.firebaseapp.com",
-databaseURL: "https://tamat-216ac.firebaseio.com",
-projectId: "tamat-216ac",
-storageBucket: "tamat-216ac.appspot.com",
-messagingSenderId: "137168516561"
+apiKey: "{Your apiKey}",
+authDomain: "{Your authDomain}",
+databaseURL: "{Your databaseURL}",
+projectId:"{Your projectId}",
+storageBucket: "{Your storageBucket}",
+messagingSenderId: "{Your messagingSenderId}"
 }
